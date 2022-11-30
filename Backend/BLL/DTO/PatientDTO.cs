551c3c3626e52c163d23a0ea334eb7bdac705e3a
a-ayeshaa <ayesha.akhtar.1999@gmail.com>
2022-12-01 00:57:08 +0600

    new

---
+++ Backend/BLL/DTO/PatientDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTO
+{
+    public class PatientDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime RegisteredAt { get; set; }
+        public string Gender { get; set; }
+        public string Phone { get; set; }
+        public string BloodGroup { get; set; }
+
+    }
+}
---
+++ Backend/DAL/EF/ASHMS_Context.cs
@@ -0,0 +1,17 @@
+﻿using DAL.EF.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class ASHMS_Context:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Patient> Patients { get; set; }
+        public DbSet<Doctor> Doctors { get; set; }
+    }
+}
