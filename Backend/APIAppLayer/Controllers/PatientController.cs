182c0c626d1027f307951ff6697452b90eb2923f
notactually-me <sameenabrar13@gmail.com>
2022-12-03 00:35:27 +0600

    Ayeshas Commit

--- Backend/APIAppLayer/Controllers/PatientController.cs
+++ Backend/APIAppLayer/Controllers/PatientController.cs
@@ -24,6 +24,35 @@ namespace APIAppLayer.Controllers
                 return Request.CreateResponse(HttpStatusCode.NotFound);
             }
         }
+        [Route("api/patients/{id}")]
+        [HttpGet]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var data = PatientServices.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
+
+        [Route("api/patients/delete/id")]
+        [HttpGet]
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                var data = PatientServices.Delete(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch
+            {
+                return Request.CreateResponse(HttpStatusCode.NotFound);
+            }
+        }
 
         [Route("api/patients/add")]
         [HttpGet]
