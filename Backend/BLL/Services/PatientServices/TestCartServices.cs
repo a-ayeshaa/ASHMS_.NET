90fb5affbae53bd06eef809eb9d901f23d03c529
Ayesha Akhtar <91655651+a-ayeshaa@users.noreply.github.com>
2022-12-04 23:45:00 +0600

    ayes

---
+++ Backend/BLL/Services/PatientServices/TestCartServices.cs
@@ -0,0 +1,71 @@
+﻿using AutoMapper;
+using BLL.DTO.PatientDTOs;
+using DAL.EF.Models;
+using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services.PatientServices
+{
+    public class TestCartServices
+    {
+        public static TestCartDTO Get(int id)
+        {
+            var data = DataAccessFactory.TestCartDataAccess().Get(id);
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestCart, TestCartDTO>();
+            });
+            var mapper = new Mapper(config);
+            var TestCart = mapper.Map<TestCartDTO>(data);
+            return TestCart;
+        }
+
+        public static List<TestCartDTO> Get()
+        {
+            var data = DataAccessFactory.TestCartDataAccess().Get();
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestCart, TestCartDTO>();
+            });
+            var mapper = new Mapper(config);
+            var TestCarts = mapper.Map<List<TestCartDTO>>(data);
+            return TestCarts;
+        }
+
+        public static TestCartDTO Add(TestCartDTO obj)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestCart, TestCartDTO>();
+                c.CreateMap<TestCartDTO, TestCart>();
+            });
+            var mapper = new Mapper(config);
+            var newobj = mapper.Map<TestCart>(obj);
+            var data = DataAccessFactory.TestCartDataAccess().Add(newobj);
+            return mapper.Map<TestCartDTO>(data);
+
+        }
+
+        public static bool Delete(int id)
+        {
+            return DataAccessFactory.TestCartDataAccess().Delete(id);
+        }
+
+        public static bool Update(TestCartDTO obj)
+        {
+            var config = new MapperConfiguration(c =>
+            {
+                c.CreateMap<TestCart, TestCartDTO>();
+                c.CreateMap<TestCartDTO, TestCart>();
+            });
+            var mapper = new Mapper(config);
+            var newobj = mapper.Map<TestCart>(obj);
+            var data = DataAccessFactory.TestCartDataAccess().Update(newobj);
+            return data;
+        }
+    }
+}
---
+++ Backend/DAL/Repos/AdminRepo/TestRepo.cs
@@ -0,0 +1,46 @@
+﻿
+using DAL.EF.Models;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Repos.AdminRepo
+{
+    internal class TestRepo:Repo,IRepo<Test,int,Test>
+    {
+
+        public Test Add(Test obj)
+        {
+            db.Tests.Add(obj);
+            db.SaveChanges();
+            return obj;
+        }
+
+        public bool Delete(int id)
+        {
+            var data = db.Tests.Find(id);
+            db.Tests.Remove(data);
+            return db.SaveChanges() > 0;
+        }
+
+        public Test Get(int id)
+        {
+            return db.Tests.Find(id);
+        }
+
+        public List<Test> Get()
+        {
+            return db.Tests.ToList();
+        }
+
+        public bool Update(Test obj)
+        {
+            var dbobj = db.Tests.Find(obj.Id);
+            db.Entry(dbobj).CurrentValues.SetValues(obj);
+            return db.SaveChanges() > 0;
+        }
+    }
+}
