8a03768a2299883f0610ed839394673c324f470f
notactually-me <sameenabrar13@gmail.com>
2022-12-09 14:24:45 +0600

    prescriptions model

---
+++ Backend/DAL/EF/Models/Prescription.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.EF.Models
+{
+    public class Prescription
+    {
+        public int Id { get; set; }
+        [Required]
+        [ForeignKey("appointment")]
+        public int Appointment_Id { get; set; }
+        public string Chief_complaint { get; set; }
+        public string On_evaluation { get; set; }
+        public string Deduction { get; set; }
+        public string Advancement { get; set; }
+
+        public virtual Appointment appointment { get; set; }
+    }
+}
