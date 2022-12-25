884688a5ce585a97a50a05c2ed694d759d3afe0c
Ayesha Akhtar <91655651+a-ayeshaa@users.noreply.github.com>
2022-12-26 03:51:46 +0600

    sameen

--- Backend/BLL/Services/DoctorServices/PrescriptionPresMedicineServices.cs
+++ Backend/BLL/Services/DoctorServices/PrescriptionPresMedicineServices.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using AutoMapper;
+using BLL.DTO.AdminDTOs;
+using BLL.DTO.DoctorDTOS;
+using DAL;
+using DAL.EF.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +13,20 @@ namespace BLL.Services.DoctorServices
 {
     public class PrescriptionPresMedicineServices
     {
+        public static List<Med_MedPresDTO> medicinePrescriptions(int pres_id)
+        {
+            var data = DataAccessFactory.MedicinePrescriptionDataAccess().Get();
+            var p = (from i in data
+                     where i.Prescription_Id==pres_id 
+                     select i).ToList();
+            var config = new MapperConfiguration(c =>
+            {
+                //c.CreateMap<Medicine, Med_MedPresDTO>();
+                c.CreateMap<MedicinePrescription, Med_MedPresDTO>();
+                c.CreateMap<Medicine, MedicineDTO>();
+            });
+            var mapper = new Mapper(config);
+            return mapper.Map<List<Med_MedPresDTO>>(p);
+        }
     }
 }
